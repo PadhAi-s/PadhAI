4bf8266c68f6015d5c5196a7c13ba6834f932593
PadhAi-s <harshthakur09082@gmail.com>
2026-08-25 13:07:12 +0530

    Connect AuthProvider to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./utils/i18n";
 import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 );
