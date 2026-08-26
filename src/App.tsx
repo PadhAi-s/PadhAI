7271465910f4be340f2273da348f649df040c669
PadhAi-s <harshthakur09082@gmail.com>
2026-08-26 11:45:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ export default function App() {
                 />
 
                 <Route
-                  path="student/ask-ai"
+                  path="student/ask"
                   element={<AskPadhAI />}
                 />
               </Route>
