75137806308eead12ebd240a302e1d0945b04603
PadhAi-s <harshthakur09082@gmail.com>
2026-08-28 00:39:02 +0530

    Update DailyNewspaper.tsx

--- src/pages/student/DailyNewspaper.tsx
+++ src/pages/student/DailyNewspaper.tsx
@@ -1,1 +1,35 @@
+<Route
+  element={
+    <ProtectedRoute allowedRole="student" />
+  }
+>
+  <Route
+    path="student/dashboard"
+    element={<StudentDashboard />}
+  />
 
+  <Route
+    path="student/profile"
+    element={<StudentProfile />}
+  />
+
+  <Route
+    path="student/syllabus"
+    element={<StudentSyllabus />}
+  />
+
+  <Route
+    path="student/ask"
+    element={<AskPadhAI />}
+  />
+
+  <Route
+    path="student/current-affairs"
+    element={<WeeklyCurrentAffairs />}
+  />
+
+  <Route
+    path="student/daily-newspaper"
+    element={<DailyNewspaper />}
+  />
+</Route>
