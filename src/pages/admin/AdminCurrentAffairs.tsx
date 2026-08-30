6b1c50d9e75ba502669763ad47321ac3b5277c45
PadhAi-s <harshthakur09082@gmail.com>
2026-08-31 00:24:20 +0530

    Update AdminCurrentAffairs.tsx

--- src/pages/admin/AdminCurrentAffairs.tsx
+++ src/pages/admin/AdminCurrentAffairs.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Papa from "papaparse";
 import { useAuth } from "../../context/AuthContext";
 import { supabase } from "../../lib/supabase";
 
